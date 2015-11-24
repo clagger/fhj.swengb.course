89a245f85e2c4020bd477187d67b79f132a132ee
knalla66 <markus.knaller@edu.fh-joanneum.at>
2015-11-24 18:57:01 +0100

    Merge branch 'master' of https://github.com/danielkandlhofer/fhj.swengb.course into danielkandlhofer-master
    # Conflicts:
    #	homework/avatarix/src/main/scala/fhj/swengb/avatarix/Avatarix.scala

--- homework/avatarix/src/main/scala/fhj/swengb/avatarix/Avatarix.scala
+++ homework/avatarix/src/main/scala/fhj/swengb/avatarix/Avatarix.scala
@@ -9,12 +9,8 @@ import javafx.scene.layout.BorderPane
 import javafx.scene.{Parent, Scene}
 import javafx.stage.Stage
 
-<<<<<<< HEAD
-import fhj.swengb.{Students, Speakers}
-=======
 
 import fhj.swengb.{Student, Speakers, Students}
->>>>>>> 911b0ecfb4e60884556b3d174a69c8138f3a0ff8
 
 import scala.util.control.NonFatal
 import scala.collection.mutable.Map
@@ -23,25 +19,6 @@ object Avatarix {
   def main(args: Array[String]) {
     Application.launch(classOf[Avatarix], args: _*)
 
-<<<<<<< HEAD
-    val x = Students.dkandlhofer.gitHubUser
-    println("Login: " + x.login)
-    println("Avatar_Url: " + x.avatarUrl.toString)
-    println("HTML_Url: " + x.html.toString)
-    println("Followers: " + x.foll)
-    println("Following: " + x.fing)
-    println("Followers URL: " + x.foll_url)
-    println("Following URL: " + x.fing_url)
-    println("created at: " + x.create)
-
-    val login = x.login
-    val foll = x.foll
-    val fing = x.fing
-    val create = x.create
-    val dictionary = Map(login -> List(foll,fing,create))
-
-    println(dictionary)
-=======
     /*
     gibt uns eine Map von Studenten mit den Parametern: Key = UserID, Value = List(Firstname,Secondname, Githubusername, AvatarUrl)
     zur Auflistung aller Students als Overview
@@ -53,7 +30,7 @@ object Avatarix {
           value.secondName,
           value.githubUsername,
           value.gitHubUser.avatarUrl.toString
-          )
+        )
       }
       data
     }
@@ -66,12 +43,12 @@ object Avatarix {
     def getStudentData(students:List[Student],x:String):List[String] = {
       val res = Nil
       for(value <- students if(value.githubUsername == x)) {
-          res ++ List(value.gitHubUser.html.toString,
-            value.gitHubUser.fing.toString,
-            value.gitHubUser.fing_url.toString,
-            value.gitHubUser.foll.toString,
-            value.gitHubUser.foll_url.toString,
-            value.gitHubUser.create.toString)
+        res ++ List(value.gitHubUser.html.toString,
+          value.gitHubUser.fing.toString,
+          value.gitHubUser.fing_url.toString,
+          value.gitHubUser.foll.toString,
+          value.gitHubUser.foll_url.toString,
+          value.gitHubUser.create.toString)
       }
       res
     }
@@ -101,10 +78,8 @@ object Avatarix {
     println(students3)
     println(test1)
 
->>>>>>> 911b0ecfb4e60884556b3d174a69c8138f3a0ff8
   }
 }
-
 
 class Avatarix extends javafx.application.Application {
 
@@ -133,22 +108,9 @@ class AvatarixController extends Initializable {
   @FXML var seppl77: BorderPane = _
 
   override def initialize(location: URL, resources: ResourceBundle): Unit = {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    //val url: String = Speakers.rladstaetter.gitHubUser.avatarUrl.toString
-    val url = Students.mknaller.gitHubUser.avatarUrl.toString
-    seppl77.setCenter(new ImageView(new Image(url)))
-=======
-    val url: String = Students.dkandlhofer.gitHubUser.avatarUrl.toString
-    //val url = Students.mfuchs.gitHubUser.avatarUrl.toString
-
-    borderPane.setCenter(new ImageView(new Image(url)))
->>>>>>> fb8ff0a00dbecc8881e670186a7ea6d063e81342
-=======
     //val url: String = Speakers.rladstaetter.gitHubUser.avatarUrl.toString
     //val url = Students.sleitner.gitHubUser.avatarUrl.toString
     //seppl77.setCenter(new ImageView(new Image(url)))
->>>>>>> 911b0ecfb4e60884556b3d174a69c8138f3a0ff8
   }
 
 }
