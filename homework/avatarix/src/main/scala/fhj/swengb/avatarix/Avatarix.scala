ed76ba943f2d83279897bbb8ed09c855e3004e9d
clagger <christian.lagger@gmail.com>
2015-11-25 20:07:26 +0100

    added gui features

--- homework/avatarix/src/main/scala/fhj/swengb/avatarix/Avatarix.scala
+++ homework/avatarix/src/main/scala/fhj/swengb/avatarix/Avatarix.scala
@@ -1,15 +1,19 @@
 package fhj.swengb.avatarix
 
+
+import javafx.scene.input.MouseEvent
+import javafx.event.EventHandler
 import java.net.URL
 import java.util.ResourceBundle
 import javafx.application.Application
 import javafx.fxml.{FXML, FXMLLoader, Initializable}
 import javafx.scene.image.{Image, ImageView}
 import javafx.scene.layout.{AnchorPane, GridPane, HBox, BorderPane}
+import javafx.scene.text.Text
 import javafx.scene.{Parent, Scene}
 import javafx.stage.Stage
 
-import fhj.swengb.Speakers
+import fhj.swengb.{Students, Speakers}
 
 import scala.util.control.NonFatal
 
@@ -22,7 +26,7 @@ object Avatarix {
 class Avatarix extends javafx.application.Application {
 
 
-  val Fxml = "/fhj/swengb/avatarix/Avatarix_Amar_Beta.fxml"
+  val Fxml = "/fhj/swengb/avatarix/lagger_testgui.fxml"
   val Css = "fhj/swengb/avatarix/Avatarix.css"
 
   val loader = new FXMLLoader(getClass.getResource(Fxml))
@@ -42,10 +46,18 @@ class Avatarix extends javafx.application.Application {
 }
 
 
+
+
+
 class AvatarixController extends Initializable {
   @FXML var border_pane: BorderPane = _
   @FXML var main_pane : AnchorPane = _
   @FXML var grid_pane : GridPane = _
+  @FXML var gitHubUser : Text = _
+  @FXML var vorname : Text = _
+  @FXML var nachname : Text = _
+  @FXML var follower : Text = _
+  @FXML var following : Text = _
 
   override def initialize(location: URL, resources: ResourceBundle): Unit = {
     //val url: String = "https://avatars0.githubusercontent.com/u/119250?v=3&s=400"
@@ -53,8 +65,6 @@ class AvatarixController extends Initializable {
     grid_pane.setHgap(10);
     grid_pane.setVgap(10);
     //grid_pane.getChildren().add(new HBox((new ImageView(new Image(url)))))
-
-
     //override def iterate()
     var gridRow = 0
     var gridColumn = 0
@@ -64,13 +74,40 @@ class AvatarixController extends Initializable {
       iv.setFitHeight(120)
       iv.setFitWidth(120)
 
+      iv.setId(i._2(2))
+     iv.setUserData(i._2)
+
       grid_pane.add(iv, gridColumn, gridRow)
+
+      iv.setOnMouseClicked(mouseEventHandler)
+
       gridColumn += 1
       if (gridColumn >= 3) {
         gridRow += 1
         gridColumn = 0
       }
     }
+
+
+
   }
+
+  val mouseEventHandler: EventHandler[_ >: MouseEvent] = new EventHandler[MouseEvent] {
+    override def handle(event: MouseEvent): Unit = {
+      event.getSource match {
+        case a: ImageView =>
+        val studenlist = Students.studentGroup1.toList
+        val detailedData:List[String] = ParserFunctions.getStudentData(studenlist, a.getId)
+
+        val data:Any = a.getUserData()
+        gitHubUser.setText(a.getId())
+
+
+
+        case _ => assert(false)
+      }
+    }
+  }
+
 
 }
