20be58b8bee3ce5a969bda7a44e6099cc390f382
Amar Bajric <amar.bajric78@gmail.com>
2015-11-25 10:12:12 +0100

    commit before merge

--- homework/avatarix/src/main/scala/fhj/swengb/avatarix/Avatarix.scala
+++ homework/avatarix/src/main/scala/fhj/swengb/avatarix/Avatarix.scala
@@ -48,12 +48,16 @@ class Avatarix extends javafx.application.Application {
 class AvatarixController extends Initializable {
   @FXML var border_pane: BorderPane = _
   @FXML var main_pane : AnchorPane = _
+  @FXML var grid_pane : GridPane = _
+
 
   override def initialize(location: URL, resources: ResourceBundle): Unit = {
     val url: String = "https://avatars0.githubusercontent.com/u/119250?v=3&s=400"
-    val url2: String = Students.abajric.gitHubUser.avatarUrl.toString
-    //grid_main_pane.add(new ImageView(new Image(url)),0,0)
-    main_pane.getChildren().addAll(new HBox(5)(url))
+    val url2: String = "https://avatars3.githubusercontent.com/u/15001225?v=3&s=460"
+    grid_pane.setHgap(10);
+    grid_pane.setVgap(10);
+    grid_pane.getChildren().add(new HBox(new ImageView(new Image(url))))
+    grid_pane.getChildren().add(new HBox(new ImageView(new Image(url2))))
 
   }
 
