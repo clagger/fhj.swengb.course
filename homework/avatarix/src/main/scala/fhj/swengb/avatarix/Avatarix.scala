611a1fb112cabe2bcfbe598a69fd594ef3c158be
clagger <christian.lagger@gmail.com>
2015-11-25 18:40:41 +0100

    added fixed size for the pictures

--- homework/avatarix/src/main/scala/fhj/swengb/avatarix/Avatarix.scala
+++ homework/avatarix/src/main/scala/fhj/swengb/avatarix/Avatarix.scala
@@ -5,7 +5,7 @@ import java.util.ResourceBundle
 import javafx.application.Application
 import javafx.fxml.{FXML, FXMLLoader, Initializable}
 import javafx.scene.image.{Image, ImageView}
-import javafx.scene.layout.BorderPane
+import javafx.scene.layout.{AnchorPane, GridPane, HBox, BorderPane}
 import javafx.scene.{Parent, Scene}
 import javafx.stage.Stage
 
@@ -22,7 +22,7 @@ object Avatarix {
 class Avatarix extends javafx.application.Application {
 
 
-  val Fxml = "/fhj/swengb/avatarix/Avatarix.fxml"
+  val Fxml = "/fhj/swengb/avatarix/Avatarix_Amar_Beta.fxml"
   val Css = "fhj/swengb/avatarix/Avatarix.css"
 
   val loader = new FXMLLoader(getClass.getResource(Fxml))
@@ -43,12 +43,34 @@ class Avatarix extends javafx.application.Application {
 
 
 class AvatarixController extends Initializable {
-  @FXML var seppl77: BorderPane = _
+  @FXML var border_pane: BorderPane = _
+  @FXML var main_pane : AnchorPane = _
+  @FXML var grid_pane : GridPane = _
 
   override def initialize(location: URL, resources: ResourceBundle): Unit = {
-    val url: String = Speakers.rladstaetter.gitHubUser.avatarUrl.toString
-    //val url = Students.mfuchs.gitHubUser.avatarUrl.toString
-    seppl77.setCenter(new ImageView(new Image(url)))
+    //val url: String = "https://avatars0.githubusercontent.com/u/119250?v=3&s=400"
+    //val url2: String = "https://avatars3.githubusercontent.com/u/15001225?v=3&s=460"
+    grid_pane.setHgap(10);
+    grid_pane.setVgap(10);
+    //grid_pane.getChildren().add(new HBox((new ImageView(new Image(url)))))
+
+
+    //override def iterate()
+    var gridRow = 0
+    var gridColumn = 0
+    for (i <- ParserFunctions.test1) {
+      val iv:ImageView = new ImageView()
+      iv.setImage(new Image(i._2(3)))
+      iv.setFitHeight(120)
+      iv.setFitWidth(120)
+
+      grid_pane.add(iv, gridColumn, gridRow)
+      gridColumn += 1
+      if (gridColumn >= 3) {
+        gridRow += 1
+        gridColumn = 0
+      }
+    }
   }
 
 }
